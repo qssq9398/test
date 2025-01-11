d30904e32e564c1aba747283284081da4b7ea3be
qssq <2960559065@qq.com>
2025-01-11 18:27:12 +0800

    提交代码

--- src/api/login.js
+++ src/api/login.js
@@ -1,5 +1,11 @@
+// 此处用于存放所有登录相关的接口请求
 import request from '@/utils/request'
-const getPicCode = () => {
-   return request.get("/captcha/image")
+
+// 1. 获取图形验证码
+export const getPicCode = () => {
+  return request.get('/captcha/image')
 }
-export { getPicCode }
+
+export const getMsgCode = () => {
+   
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login">
     <van-nav-bar title="会员登录" left-arrow @click-left="$router.go(-1)" />
+
     <div class="container">
       <div class="title">
         <h3>手机号登录</h3>
@@ -9,93 +10,130 @@
 
       <div class="form">
         <div class="form-item">
-          <input
-            class="inp"
-            maxlength="11"
-            placeholder="请输入手机号码"
-            type="text"
-          />
+          <input v-model="mobile" class="inp" maxlength="11" placeholder="请输入手机号码" type="text">
         </div>
         <div class="form-item">
-          <input
-            class="inp"
-            maxlength="5"
-            placeholder="请输入图形验证码"
-            type="text"
-          />
-          <img v-if="picUrl" :src="picUrl" alt="" @click="getPicCode" />
+          <input v-model="picCode" class="inp" maxlength="5" placeholder="请输入图形验证码" type="text">
+          <img v-if="picUrl" :src="picUrl" @click="getPicCode" alt="">
         </div>
         <div class="form-item">
-          <input class="inp" placeholder="请输入短信验证码" type="text" />
-          <button @click="sendCode">
-            {{
-              totalSecond === CurrentSecond
-                ? "获取验证码"
-                : CurrentSecond + "秒后重新发送"
-            }}
+          <input v-model="msgCode" class="inp" placeholder="请输入短信验证码" type="text">
+          <button @click="getCode">
+            {{ second === totalSecond ? '获取验证码' : second + '秒后重新发送'}}
           </button>
         </div>
       </div>
 
-      <div class="login-btn">登录</div>
+      <div @click="login" class="login-btn">登录</div>
     </div>
   </div>
 </template>
 
 <script>
-import { getPicCode } from "@/api/login";
+import { getPicCode, getMsgCode } from '@/api/login'
+// import { Toast } from 'vant'
 
 export default {
-  name: "LoginPage",
-  data() {
+  name: 'LoginPage',
+  data () {
     return {
-      picCode: "",
-      picUrl: "",
-      picKey: "",
-      totalSecond: 60,
-      CurrentSecond: 60,
-      timer: null, // 定时器 ID
-    };
-  },
-  created() {
-    this.getPicCode();
-  },
-  beforeDestroy() {
-    // 清除定时器
-    if (this.timer) {
-      clearInterval(this.timer);
+      picKey: '', // 将来请求传递的图形验证码唯一标识
+      picUrl: '', // 存储请求渲染的图片地址
+      totalSecond: 60, // 总秒数
+      second: 60, // 当前秒数，开定时器对 second--
+      timer: null, // 定时器 id
+      mobile: '', // 手机号
+      picCode: '', // 用户输入的图形验证码
+      msgCode: '' // 短信验证码
     }
   },
+  async created () {
+    this.getPicCode()
+  },
   methods: {
-    async getPicCode() {
-      const {
-        data: { base64, key },
-      } = await getPicCode();
-      this.picUrl = base64;
-      this.picKey = key;
+    // 获取图形验证码
+    async getPicCode () {
+      const { data: { base64, key } } = await getPicCode()
+      this.picUrl = base64 // 存储地址
+      this.picKey = key // 存储唯一标识
+
+      // Toast('获取图形验证码成功')
+      // this.$toast('获取成功')
+      // this.$toast.success('成功文案')
     },
-    sendCode() {
-      if (this.CurrentSecond !== this.totalSecond) {
-        // 如果倒计时尚未结束，禁止重复点击
-        return;
+
+    // 校验 手机号 和 图形验证码 是否合法
+    // 通过校验，返回true
+    // 不通过校验，返回false
+    validFn () {
+      if (!/^1[3-9]\d{9}$/.test(this.mobile)) {
+        this.$toast('请输入正确的手机号')
+        return false
+      }
+      if (!/^\w{4}$/.test(this.picCode)) {
+        this.$toast('请输入正确的图形验证码')
+        return false
+      }
+      return true
+    },
+
+    // 获取短信验证码
+    async getCode () {
+      if (!this.validFn()) {
+        return
       }
 
-      // 开始倒计时
-      this.timer = setInterval(() => {
-        console.log("正在倒计时");
-        if (this.CurrentSecond > 0) {
-          this.CurrentSecond--;
-        } else {
-          clearInterval(this.timer); // 清除定时器
-          this.timer = null; // 重置定时器 ID
-          this.CurrentSecond = this.totalSecond; // 重置倒计时
-        }
-      }, 1000);
-    },
-  },
-};
-</script>
+      // 当前目前没有定时器开着，且 totalSecond 和 second 一致 (秒数归位) 才可以倒计时
+      if (!this.timer && this.second === this.totalSecond) {
+        // 发送请求
+        // 预期：希望如果响应的status非200，最好抛出一个promise错误，await只会等待成功的promise
+        await getMsgCode(this.picCode, this.picKey, this.mobile)
 
+        this.$toast('短信发送成功，注意查收')
+
+        // 开启倒计时
+        this.timer = setInterval(() => {
+          this.second--
+
+          if (this.second <= 0) {
+            clearInterval(this.timer)
+            this.timer = null // 重置定时器 id
+            this.second = this.totalSecond // 归位
+          }
+        }, 1000)
+      }
+    },
+
+    // 登录
+    async login () {
+      if (!this.validFn()) {
+        return
+      }
+
+      if (!/^\d{6}$/.test(this.msgCode)) {
+        this.$toast('请输入正确的手机验证码')
+        return
+      }
+
+      console.log('发送登录请求')
+
+      const res = await codeLogin(this.mobile, this.msgCode)
+      this.$store.commit('user/setUserInfo', res.data)
+      this.$toast('登录成功')
+
+      // 进行判断，看地址栏有无回跳地址
+      // 1. 如果有   => 说明是其他页面，拦截到登录来的，需要回跳
+      // 2. 如果没有 => 正常去首页
+      const url = this.$route.query.backUrl || '/'
+      this.$router.replace(url)
+    }
+  },
+  // 离开页面清除定时器
+  beforedestroyed () {
+    clearInterval(this.timer)
+  }
+}
+</script>
 
 <style lang="less" scoped>
 .container {
@@ -146,10 +184,10 @@ export default {
     width: 100%;
     height: 42px;
     margin-top: 39px;
-    background: linear-gradient(90deg, #ecb53c, #ff9211);
+    background: linear-gradient(90deg,#ecb53c,#ff9211);
     color: #fff;
     border-radius: 39px;
-    box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.1);
+    box-shadow: 0 10px 20px 0 rgba(0,0,0,.1);
     letter-spacing: 2px;
     display: flex;
     justify-content: center;
